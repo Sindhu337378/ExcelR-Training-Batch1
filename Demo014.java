7e35e7fb0d065d03addf5ae66a709627baf7dab8
Sindhu337378 <rishinagunoori21@gmail.com>
2025-01-07 13:17:39 +0530

    Add files via upload

---
+++ Demo014.java
@@ -0,0 +1,19 @@
+class Demo014{
+    public static void main(String args[]){
+        int a = 10;
+        System.out.println("Initial value of a: "+a);
+
+        System.out.println("post-increment (a++): "+(a++));
+        System.out.println("Value of a after post-increment:"+a);
+
+        System.out.println("Pre-increment (++a):"+ ++a);
+        System.out.println("Value of a after pre-increment:"+a);
+
+        System.out.println("post-decrement (a--):"+ --a);
+        System.out.println("Value of a after post-decrement"+a);
+
+        System.out.println("pre-decrement (--a):"+ --a);
+        System.out.println("Value of a after pre-decrement"+a);
+
+    }
+}
