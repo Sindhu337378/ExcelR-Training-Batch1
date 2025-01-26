3ade6b725ef8bb7c6d6a3b1c6c86863d41b3ea6f
Sindhu337378 <rishinagunoori21@gmail.com>
2025-01-26 20:08:56 +0530

    Delete Demo013.java

--- Demo013.java
+++
@@ -1,9 +0,0 @@
-class Demo013 {
-    public static void main(String args[]){
-        int base = 20;
-        int height = 30;
-        double area = (0.5)*base*height;
-        System.out.println(area);
-    }
-
-}
