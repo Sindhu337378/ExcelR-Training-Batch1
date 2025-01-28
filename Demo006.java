d7515d2d937036121d5349b7978bbb67c2fcc930
Sindhu337378 <rishinagunoori21@gmail.com>
2025-01-28 11:10:30 +0530

    string variables

---
+++ Demo006.java
@@ -0,0 +1,13 @@
+class Demo006 {
+   Demo006() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "sindhu_001";
+      String var2 = "sindu_001";
+      if (var1 == "sindhu_001" && var2 == "sindu_001") {
+         System.out.println("hello world");
+        }
+
+}
+}
